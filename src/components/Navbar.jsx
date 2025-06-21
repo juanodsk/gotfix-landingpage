2f7422879e278474fa1729c9b7d2981a6ef2c9d4
juanodsk <juanph.dv@gmail.com>
2025-06-21 17:37:08 -0500

    nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,26 +4,42 @@ import Logo from "../assets/Logo.png"; // Adjust the path as necessary
 const Navbar = () => {
   return (
     <nav>
-      <div className="bg-[#00162b] container mx-auto px-4 py-2 w-full">
-        <div className="flex justify-between items-center">
-          <img src={Logo} alt="" />
-          <ul className="flex space-x-4">
-            <li>
-              <a href="/" className="text-gray-600 hover:text-gray-800">
-                Inicio
-              </a>
-            </li>
-            <li>
-              <a href="/about" className="text-gray-600 hover:text-gray-800">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/contact" className="text-gray-600 hover:text-gray-800">
-                Contact
-              </a>
-            </li>
-          </ul>
+      <div className="bg-[#00162b] container mx-auto px-4 py-2 w-full rounded-xl mt-4">
+        <div className="flex  items-center">
+          <div className="flex-none">
+            <img src={Logo} alt="Logo Gotfix" className="w-75" />
+          </div>
+          <div
+            className="
+            flex-auto flex justify-center items-center space-x-4 text-lg "
+          >
+            <ul className="flex space-x-4 ">
+              <li>
+                <a
+                  href="/"
+                  className="text-white  transition delay-10 hover:text-[#0087fa]"
+                >
+                  Inicio
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/about"
+                  className="text-white  transition delay-10 hover:text-[#0087fa]"
+                >
+                  Servicios
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/contact"
+                  className="text-white transition delay-10 hover:text-[#0087fa]"
+                >
+                  Contactanos
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
